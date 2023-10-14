34d183f81e7fe36dfba71515b192686afccd93f7
vivaldev <valtteri.virtanen.dev@gmail.com>
2023-10-14 16:01:52 +0300

    simple number guessing game in python

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ guess_limit = 10
 
 while running:
     guess = int(input("Enter an integer: "))
-
     if guess == number:
         print("You guessed it!")
         # this causes the while loop and program to stop
         running = False
     elif guess < number:
-        print("No, it's a little higher than that...")
+        print("No, it's a little higher than that...\n")
         guess_limit -= 1
-    elif guess == 0 or guess > 100:
+    elif guess > number:
+        print("No, it's a little lower than that...")
         guess_limit -= 1
     else:
-        print("No, it's a little higher than that...")
+        print("Invalid input. Please enter a number between 1 and 100.")
         guess_limit -= 1
 else:
     print("The while loop is over!")
-print("Guess the number between 1 - 100 only, please!")
-print("Done!")
+    print("Guess the number between 1 - 100 only, please!")
+    print("Done!")
